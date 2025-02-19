f83c72afb14b12a7e2236d68d6538eea77d38f9f
freydisxuan <freydisxuan@gmail.com>
2025-02-19 17:45:27 +0000

    nyoom

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,7 @@
-CREATE TABLE IF NOT EXISTS public.categories (
+CREATE TABLE IF NOT EXISTS categories (
   id serial primary key,
   name varchar(64) not null unique,
   created timestamp with time zone not null default current_timestamp
 );
+
+CREATE TABLE IF NOT EXISTS 
